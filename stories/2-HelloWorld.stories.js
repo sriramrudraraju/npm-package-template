32b9a121c1628bab0cb13b725b463501827c511e
sriramrudraraju <sriram.rudraraju6@gmail.com>
2019-09-19 13:30:50 -0500

    feat: storybook integration

---
+++ stories/2-HelloWorld.stories.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import HelloWorld from '../src/hello-world/hello-world.component';
+
+export default {
+  title: 'Hello World',
+};
+
+export const display = () => (
+  <HelloWorld />
+);
