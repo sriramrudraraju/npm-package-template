d0eb0702bb1af267f8c0a180e4a1c7b80c94925d
Sriram Rudraraju <sriram.rudraraju6@gmail.com>
2019-10-10 13:36:03 -0500

    update storybook webpack config

--- .storybook/webpack.config.js
+++ .storybook/webpack.config.js
@@ -1,12 +1,17 @@
 module.exports = ({ config }) => {
+  /**
+   * sourceType config might change in future,
+   * issue: https://github.com/storybookjs/storybook/issues/3346
+   */
+  config.module.rules[0].use[0].options.sourceType = "unambiguous";
+  
   config.module.rules.push({
     test: /\.(ts|tsx)$/,
-    use: [
-      {
-        loader: require.resolve('awesome-typescript-loader'),
-      }
-    ],
+    loader: require.resolve('babel-loader'),
+    options: {
+      presets: [['react-app', { flow: false, typescript: true }]],
+    },
   });
   config.resolve.extensions.push('.ts', '.tsx');
   return config;
-  };
+};
